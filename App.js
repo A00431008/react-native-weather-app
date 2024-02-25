062b35a9b040f157f54f1f1e18d0a3261abff6e7
A00431008 <Sudeep.Raj.Badal@smu.ca>
2024-02-25 00:27:21 -0400

    Added Header with app name

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from "@react-navigation/native";
+import { Header, Icon } from "@rneui/base";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import CurrentLocationScreen from './Screens/CurrentLocationScreen';
 import SearchScreen from './Screens/SearchScreen';
@@ -13,6 +15,18 @@ export default function App() {
   return (
     <>
       <StatusBar />
+      <Header
+        backgroundImageStyle={{}}
+        barStyle="default"
+        centerComponent={{
+          text: "WEATHER APP",
+          style: { color: "#fff" }
+        }}
+        centerContainerStyle={{marginTop: "5%"}}
+        containerStyle={{ width: "100%", paddingTop:"30"}}
+        placement="center"
+        statusBarProps={{}}
+      />
       <NavigationContainer>
         <Tab.Navigator>
             <Tab.Screen 
